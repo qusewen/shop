fb0c37831ddba4d2e6ab2329d56787815cc77559
George7836 <96310979+George7836@users.noreply.github.com>
2022-12-29 22:14:44 +0300

    feat: add header bar

--- shop/src/Components/Header/Header.tsx
+++ shop/src/Components/Header/Header.tsx
@@ -1,9 +1,25 @@
+import { ShoppingBasket } from '@mui/icons-material'
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import AppleIcon from '@mui/icons-material/Apple';
 import React from 'react'
+import './Header.scss'
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div>Header</div>
+    <AppBar color='default'>
+      <Toolbar className='nav-bar'>
+        <IconButton 
+            size="large"
+            edge="start"
+            color="inherit">
+              <AppleIcon/>
+        </IconButton>
+        <IconButton color='inherit'>
+              <ShoppingBasket/>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   )
 }
